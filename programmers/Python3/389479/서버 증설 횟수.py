5264f92ecb0d21b74d795f4fde8fb1025d671051
SEON YEONG <167170974+sunz8281@users.noreply.github.com>
2026-05-08 12:15:31 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.07 ms, Memory: 9.1 MB -BaekjoonHub

--- programmers/Python3/389479/서버 증설 횟수.py
+++ programmers/Python3/389479/서버 증설 횟수.py
@@ -1,13 +1,10 @@
 def solution(players, m, k):
     servers = [[0, 0] for i in range(len(players))]
+    cnt = 0
     for i in range(len(players)):
         add = max(0, players[i]//m-servers[i][0])
-        servers[i][1] = add
+        cnt += add
         for j in range(k):
             if i+j>=len(players): continue
             servers[i+j][0] += add
-    print(servers)
-    cnt = 0
-    for server in servers:
-        cnt += server[1]
     return cnt
